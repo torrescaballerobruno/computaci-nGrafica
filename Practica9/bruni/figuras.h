7d7f0614838d20282270d6357b2b971441008ebd
Bruno Torres <bruno.torres@gm3s.com.mx>
2019-04-09 10:38:30 -0500

    practica9

---
+++ Practica9/bruni/figuras.h
@@ -0,0 +1,229 @@
+//#include <Windows.h>
+#include <GL/glut.h>     
+#include <stdio.h>	
+#include <string.h>
+#include <math.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+const double PI = 3.1415926535897;
+float text_der = 1.0;
+float text_izq = 0.0;
+
+
+void esfera(GLfloat radio, int meridianos, int paralelos, GLuint text )
+{
+	GLdouble theta, phi;
+
+	float ctext_s = 1.0/meridianos;
+	float ctext_t = 1.0/paralelos;
+
+	glBindTexture(GL_TEXTURE_2D, text);   // choose the texture to use.
+
+	float v1[]={0.0, 0.0, 0.0};
+	float v2[]={0.0, 0.0, 0.0};
+	float v3[]={0.0, 0.0, 0.0};
+	float v4[]={0.0, 0.0, 0.0};
+	int i, j;
+	float angulom, angulop;
+	angulom=2*3.141592654/meridianos;
+	angulop=3.141592654/paralelos; 
+	for(i=0;i<meridianos;i++)
+	{
+		for(j=0;j<paralelos;j++)
+		{
+			v1[0]=radio*cos(angulom*i)*sin(angulop*j);
+			v1[1]=radio*cos(angulop*j);
+			v1[2]=radio*sin(angulom*i)*sin(angulop*j);
+			
+			v2[0]=radio*cos(angulom*i)*sin(angulop*(j+1));
+			v2[1]=radio*cos(angulop*(j+1));
+			v2[2]=radio*sin(angulom*i)*sin(angulop*(j+1));
+			
+			v3[0]=radio*cos(angulom*(i+1))*sin(angulop*(j+1));
+			v3[1]=radio*cos(angulop*(j+1));
+			v3[2]=radio*sin(angulom*(i+1))*sin(angulop*(j+1));
+
+			v4[0]=radio*cos(angulom*(i+1))*sin(angulop*j);
+			v4[1]=radio*cos(angulop*j);
+			v4[2]=radio*sin(angulom*(i+1))*sin(angulop*j);
+			
+			glBegin(GL_POLYGON);
+				glNormal3fv(v1);
+				glTexCoord2f(ctext_s*i, -ctext_t*j);	
+				glVertex3fv(v1);
+				glNormal3fv(v2);
+				glTexCoord2f(ctext_s*i, -ctext_t*(j+1));	
+				glVertex3fv(v2);
+				glNormal3fv(v3);
+				glTexCoord2f(ctext_s*(i+1), -ctext_t*(j+1));	
+				glVertex3fv(v3);
+				glNormal3fv(v4);
+				glTexCoord2f(ctext_s*(i+1), -ctext_t*j);	
+				glVertex3fv(v4);
+			glEnd();
+		}
+	}
+}
+
+void torus(GLfloat radioM, GLfloat radiom, int meridianos, int paralelos )
+{
+
+	float R = 0;
+	float r = 0;
+
+	float v1[]={0.0, 0.0, 0.0};
+	float v2[]={0.0, 0.0, 0.0};
+	float v3[]={0.0, 0.0, 0.0};
+	float v4[]={0.0, 0.0, 0.0};
+
+	int i,j;
+
+	GLdouble angulop = 2*3.1415/paralelos;
+	GLdouble angulom = 2*3.1415/meridianos;
+
+	r = (radioM - radiom)/2;
+	R = radiom + r;
+
+	for(i=0;i<meridianos;i++)
+	{
+		for(j=0;j<paralelos;j++)
+		{
+			
+			v1[0]=R*cos(angulom*i) + r*cos(angulom*i)*cos(angulop*j);
+			v1[2]=R*sin(angulom*i)+ r*sin(angulom*i)*cos(angulop*j);
+			v1[1]=r*sin(angulop*j);
+
+			glNormal3f(v1[0], v1[1], v1[2]);
+			
+			v2[0]=R*cos(angulom*i) + r*cos(angulom*i)*cos(angulop*(j+1));
+			v2[2]=R*sin(angulom*i) + r*sin(angulom*i)*cos(angulop*(j+1));
+			v2[1]=r*sin(angulop*(j+1));
+
+			glNormal3f(v2[0], v2[1], v2[2]);
+
+			v3[0]=R*cos(angulom*(i+1)) + r*cos(angulom*(i+1))*cos(angulop*(j+1));
+			v3[2]=R*sin(angulom*(i+1)) + r*sin(angulom*(i+1))*cos(angulop*(j+1));
+			v3[1]=r*sin(angulop*(j+1));
+
+			glNormal3f(v3[0], v3[1], v3[2]);
+
+			v4[0]=R*cos(angulom*(i+1)) + r*cos(angulom*(i+1))*cos(angulop*j);
+			v4[2]=R*sin(angulom*(i+1)) + r*sin(angulom*(i+1))*cos(angulop*j);
+			v4[1]=r*sin(angulop*j);
+
+			glNormal3f(v4[0], v4[1], v4[2]);
+			
+			glBegin(GL_POLYGON);
+				glVertex3fv(v1);
+				glVertex3fv(v2);
+				glVertex3fv(v3);
+				glVertex3fv(v4);
+			glEnd();
+		}
+	}
+}
+
+void cono(float altura, float radio, int resolucion, GLuint text)
+{
+	
+	float v1[]={0.0, 0.0, 0.0};
+	float v2[]={0.0, 0.0, 0.0};
+	float v3[]={0.0, 0.0, 0.0};
+	float v4[]={0.0, 0.0, 0.0};
+
+    int i;
+	float angulo=2*3.14/resolucion;
+	float ctext_s = 1.0/resolucion;
+	
+	glBindTexture(GL_TEXTURE_2D, text);   // choose the texture to use.
+	for(i=0; i <resolucion; i++)
+	{
+		
+		v2[0]=radio*cos(angulo*i);
+		v2[1]=0;
+		v2[2]=radio*sin(angulo*i);
+
+		v3[0]=radio*cos(angulo*(i+1));
+		v3[1]=0;
+		v3[2]=radio*sin(angulo*(i+1));
+		
+		//glBegin(GL_LINE_LOOP);
+        glBegin(GL_POLYGON); //Construye Base
+			glNormal3f( 0.0f, -1.0f, 0.0f);
+			glVertex3f(0.0,0.0,0.0);
+			glVertex3fv(v2);
+			glVertex3fv(v3);
+		glEnd();
+
+
+		glBindTexture(GL_TEXTURE_2D, text);
+		glBegin(GL_TRIANGLES);
+			glNormal3f( v2[0], 0.0f, v2[2]);
+			glTexCoord2f(ctext_s*i, 0.0f);		glVertex3fv(v2);
+			glTexCoord2f(ctext_s*(i+1), 0.0f);	glVertex3fv(v3);
+			glTexCoord2f(0.5, 1.0f);	glVertex3f(0.0,altura,0.0);
+		glEnd();
+	}
+}
+
+void cilindro(float radio, float altura, int resolucion, GLuint text)
+{
+	
+	float v1[]={0.0, 0.0, 0.0};
+	float v2[]={0.0, 0.0, 0.0};
+	float v3[]={0.0, 0.0, 0.0};
+	float v4[]={0.0, 0.0, 0.0};
+	float v5[]={0.0, 0.0, 0.0};
+
+    int i;
+	float angulo=2*3.14/resolucion;
+
+	//float ctext_s = 1/resolucion-1;
+	float ctext_s = 1.0/resolucion;
+	float ctext_t = 0.0;
+
+	
+	for(i=0; i <resolucion; i++)
+	{
+		 
+		v2[0]=radio*cos(angulo*i);
+		v2[1]=0;
+		v2[2]=radio*sin(angulo*i);
+
+		v3[0]=radio*cos(angulo*(i+1));
+		v3[1]=0;
+		v3[2]=radio*sin(angulo*(i+1));
+	
+		v4[0]=radio*cos(angulo*i);
+		v4[1]=altura;
+		v4[2]=radio*sin(angulo*i);
+
+		v5[0]=radio*cos(angulo*(i+1));
+		v5[1]=altura;
+		v5[2]=radio*sin(angulo*(i+1));
+
+		glBegin(GL_POLYGON);
+			glNormal3f( 0.0f, -1.0f, 0.0f);
+			glVertex3f(0.0,0.0,0.0);
+			glVertex3fv(v2);
+			glVertex3fv(v3);
+		glEnd();
+
+		glBegin(GL_POLYGON);
+			glNormal3f( 0.0f, 1.0f, 0.0f);
+			glVertex3f(0.0,altura,0.0);
+			glVertex3fv(v4);
+			glVertex3fv(v5);
+		glEnd();
+
+		glBindTexture(GL_TEXTURE_2D, text);
+		glBegin(GL_POLYGON);
+			glNormal3f( v2[0], 0.0f, v2[2]);
+			glTexCoord2f(ctext_s*i, 0.0f);		glVertex3fv(v2);
+			glTexCoord2f(ctext_s*(i+1), 0.0f);	glVertex3fv(v3);
+			glTexCoord2f(ctext_s*(i+1), 1.0f);	glVertex3fv(v5);
+			glTexCoord2f(ctext_s*i, 1.0f);		glVertex3fv(v4);
+		glEnd();
+	}
+}	
